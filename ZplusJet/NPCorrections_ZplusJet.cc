da439d32d25f46d1b23a87ae77433e2500516bb9
HerrHorizontal <maximilian.horzela@student.kit.edu>
2019-11-27 11:27:53 +0100

    added phistareta and jet cut + nicer histogram declarations

--- ZplusJet/NPCorrections_ZplusJet.cc
+++ ZplusJet/NPCorrections_ZplusJet.cc
@@ -20,67 +20,70 @@ namespace Rivet {
     /// Book histograms and initialise projections before the run
     void init() {
 
-      // Initialise and register projections
+      /// Initialise and register projections
       const FinalState ffs(Cuts::abseta < 5 && Cuts::pT > 100*MeV);
       const ChargedFinalState cfs(ffs);
       declare(ffs, "FS");
       declare(cfs, "CFS");
-      // leptons
+      /// leptons
       PromptFinalState bare_leps(Cuts::abspid == PID::MUON || Cuts::abspid == PID::ELECTRON);
       declare(bare_leps, "bare_leptons");
       PromptFinalState photons(Cuts::abspid == PID::PHOTON);
       DressedLeptons dressed_leps(photons, bare_leps, 0.1, Cuts::abseta < 2.5 && Cuts::pT > 10*GeV);
       declare(dressed_leps, "dressed_leptons");
-      // jet collection
+      /// jet collection
       FastJets jets(ffs, FastJets::ANTIKT, 0.4, JetAlg::Invisibles::NONE)
       declare(jets, "Jets");
 
-      // out of acceptance particles treat as invisible
+      /// out of acceptance particles treat as invisible
       VetoedFinalState fs_onlyinacc(ffs, (Cuts::abspid::MUON && Cuts::abseta > 2.4) || 
                                     (Cuts::abspid::PHOTON && Cuts::abseta > 3.0) || 
                                     (Cuts::abspid::ELECTRON && Cuts::abseta > 3.0));
       declare(MissingMomentum(fs_onlyinacc), "invisibles");
 
-      // Book histograms
+      /// Book histograms
       //// Book histograms with equidistant bins
-      _hist_NMus = bookHisto1D("NMuons", 10, 0, 10);
-      _hist_MuPlusPt = bookHisto1D("MuPlusPt", 11, 25, 300);
-      _hist_MuPlusEta = bookHisto1D("MuPlusEta", 24, -2.4, 2.4);
-      _hist_MuPlusPhi = bookHisto1D("MuPlusPhi", 30, -3.14, 3.14);
-      _hist_MuMinusPt = bookHisto1D("MuMinusPt", 11, 25, 300);
-      _hist_MuMinusEta = bookHisto1D("MuMinusEta", 24, -2.4, 2.4);
-      _hist_MuMinusPhi = bookHisto1D("MuMinusPhi", 30, -3.14, 3.14);
-      _hist_ZY = bookHisto1D("ZY", 12, -2.4, 2.4);
-      _hist_ZM = bookHisto1D("ZM", 20, 71, 111);
-      _hist_ZPhi = bookHisto1D("ZPhi", 30, -3.14159, 3.14159);
-      _hist_NJets = bookHisto1D("NJets", 10, 0, 10);
-      _hist_Jet1Y = bookHisto1D("Jet1Y", 12, -2.4, 2.4);
-      _hist_Jet1Phi = bookHisto1D("Jet1Phi", 31, -3.142, 3.142);
-      _hist_Jet2Y = bookHisto1D("Jet2Y", 30, -2.4, 2.4);
-      _hist_Jet2Phi = bookHisto1D("Jet2Phi", 31, -3.142, 3.142);
-      _hist_Jet3Y = bookHisto1D("Jet3Y", 30, -2.4, 2.4);
-      _hist_Jet3Phi = bookHisto1D("Jet1Phi", 31, -3.142, 3.142);
-      _hist_MET = bookHisto1D("MET", 70, 0, 350);
+      book(_h["NMus"], "N_Muons", 10, 0, 10);
+      //_hist_NMus = bookHisto1D("NMuons", 10, 0, 10);
+      book(_h["MuPlusPt"], "pT_Mu+", 11, 25, 300);
+      book(_h["MuPlusEta"], "eta_Mu+", 24, -2.4, 2.4);
+      book(_h["MuPlusPhi"], "phi_Mu+", 30, -3.14, 3.14);
+      book(_h("MuMinusPt"), "pT_Mu-", 11, 25, 300);
+      book(_h["MuMinusEta"], "eta_Mu-", 24, -2.4, 2.4);
+      book(_h["MuMinusPhi"], "phi_Mu-", 30, -3.14, 3.14);
+      book(_h["ZY", "y_Z", 12, -2.4, 2.4);
+      book(_h["ZM"], "m_Z", 20, 71, 111);
+      book(_h["ZPhi"], "phi_Z", 30, -3.14159, 3.14159);
+      book(_h["NJets"], "N_Jets", 10, 0, 10);
+      book(_h["Jet1Y"], "y_Jet1", 12, -2.4, 2.4);
+      book(_h["Jet1Phi"], "phi_Jet1", 31, -3.142, 3.142);
+      book(_h["Jet2Y"], "y_Jet2", 30, -2.4, 2.4);
+      book(_h["Jet2Phi"], "phi_Jet2", 31, -3.142, 3.142);
+      book(_h["Jet3Y"], "y_Jet3", 30, -2.4, 2.4);
+      book(_h["Jet3Phi"], "phi_Jet3", 31, -3.142, 3.142);
+      book(_h["MET"], "MET", 70, 0, 350);
 
       //// Book histograms with variable bin size
-      ////// Define bin edges
-      std::vector<double> binedges_ZPt = {25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110, 130, 150, 170, 190, 220, 250, 400, 1000};
+      ///// Define bin edges
+      vector<double> binedges_ZPt = {25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 110, 130, 150, 170, 190, 220, 250, 400, 1000};
 
-      std::vector<double> binedges_Jet1Pt = {20, 25, 30, 40, 50, 75, 125, 175, 225, 300, 400, 500, 700, 1000};
-      std::vector<double> binedges_Jet2Pt = {5, 10, 20, 30, 40, 50, 75, 125, 175, 250, 400};
-      std::vector<double> binedges_Jet3Pt = {5, 10, 20, 30, 40, 50, 75, 125, 175, 250, 400};
-      std::vector<double> binedges_JetAvePt = {5, 10, 15, 20, 25, 30, 40, 50, 75, 125, 175, 225, 300, 400};
-      std::vector<double> binedges_Jet1Eta = {-5.191, -3.839, -3.489, -3.139, -2.964, -2.853, -2.650, -2.500, -2.322, -2.172, -1.930,
+      vector<double> binedges_Jet1Pt = {20, 25, 30, 40, 50, 75, 125, 175, 225, 300, 400, 500, 700, 1000};
+      vector<double> binedges_Jet2Pt = {5, 10, 20, 30, 40, 50, 75, 125, 175, 250, 400};
+      vector<double> binedges_Jet3Pt = {5, 10, 20, 30, 40, 50, 75, 125, 175, 250, 400};
+      vector<double> binedges_JetAvePt = {5, 10, 15, 20, 25, 30, 40, 50, 75, 125, 175, 225, 300, 400};
+      vector<double> binedges_Jet1Eta = {-5.191, -3.839, -3.489, -3.139, -2.964, -2.853, -2.650, -2.500, -2.322, -2.172, -1.930,
                                              -1.653, -1.479, -1.305, -1.044, -0.783, -0.522, -0.261, 0.000, 0.261, 0.522, 0.783, 1.044, 
                                              1.305, 1.479, 1.653, 1.930, 2.172, 2.322, 2.500, 2.650, 2.853, 2.964, 3.139, 3.489, 3.839, 
                                              5.191};
-      ////// Book Histograms with vector
-      _hist_ZPt = bookHisto1D("ZPt", binedges_ZPt);
-      _hist_Jet1Pt = bookHisto1D("ZPt", binedges_Jet1Pt);
-      _hist_Jet2Pt = bookHisto1D("ZPt", binedges_Jet2Pt);
-      _hist_Jet3Pt = bookHisto1D("ZPt", binedges_Jet3Pt);
-      _hist_JetAvePt = bookHisto1D("ZPt", binedges_JetAvePt);
-      _hist_Jet1Eta = bookHisto1D("ZPt", binedges_Jet1Eta);
+      vector<double> binedges_PhiStarEta = {0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2, 3, 4, 5, 7, 10, 15, 20, 30, 50};
+      ///// Book Histograms with vector
+      book(_h["ZPt"], "pT_Z", binedges_ZPt);
+      book(_h["Jet1Pt"], "pZ_Jet1", binedges_Jet1Pt);
+      book(_h["Jet2Pt"], "pT_Jet2", binedges_Jet2Pt);
+      book(_h["Jet3Pt"], "pT_Jet3", binedges_Jet3Pt);
+      book(_h["JetAvePt"], "<pT>_Jets", binedges_JetAvePt);
+      book(_h["Jet1Eta"], "eta_Jet1", binedges_Jet1Eta);
+      book(_h["PhiStarEta"], "Phi*_eta", binedges_PhiStarEta);
 
     }
 
@@ -88,12 +91,17 @@ namespace Rivet {
     /// Perform the per-event analysis
     void analyze(const Event& event) {
 
-      /// @todo Do the event by event analysis here
-      //// Find muons and jets
+      /// Find muons and jets
       vector<DressedLepton> muons = apply<DressedLeptons>(event, "dressed_leptons").dressedLeptons(Cuts::abspid::MUON && Cuts::abseta < 2.4 && Cuts::pT > 25*GeV);
       Jets jets = apply<JetAlg>(event, "Jets").jetsByPt(Cuts::absrap < 2.4 && Cuts::pT > 0.1*GeV);
 
-      //// Require at least two opposite sign muons compatible with Z-boson mass and keep the pair closest to Zboson mass
+      /// Remove jet-muon overlap
+      idiscardIfAnyDeltaRLess(jets, muons, 0.3);
+
+      /// Require at least one hard jet
+      if (jets.at(0).pT() <= 20*GeV) vetoEvent;
+
+      /// Require at least two opposite sign muons compatible with Z-boson mass and keep the pair closest to Zboson mass
       bool _bosoncandidateexists = False;
       double _massdiff = 20*GeV;
       DressedLepton _muon;
@@ -122,59 +130,61 @@ namespace Rivet {
 
       if (!(_bosoncandidateexists)) vetoEvent;
 
-      //// Fill muon related histograms
-      _hist_NMus -> fill(muons.size());
+      /// Fill muon related histograms
+      _h["NMus"] -> fill(muons.size());
 
-      double phi_Mu = _muon.phi();
-      _hist_MuMinusPhi -> fill(phi_Mu);
-      double phi_AntiMu = _antimuon.phi();
-      _hist_MuPlusPhi -> fill(phi_AntiMu);
+      const double phi_Mu = _muon.phi();
+      _h["MuMinusPhi"] -> fill(phi_Mu);
+      const double phi_AntiMu = _antimuon.phi();
+      _h["MuPlusPhi"] -> fill(phi_AntiMu);
 
-      double eta_Mu = _muon.eta();
-      _hist_MuMinusEta -> fill(eta_Mu);
-      double eta_AntiMu = _antimuon.eta();
-      _hist_MuPlusEta -> fill(eta_AntiMu);
+      const double eta_Mu = _muon.eta();
+      _h["MuMinusEta"] -> fill(eta_Mu);
+      const double eta_AntiMu = _antimuon.eta();
+      _h["MuPlusEta"] -> fill(eta_AntiMu);
 
-      double pT_Mu = _muon.pT()/GeV;
-      _hist_MuMinusPt -> fill(pT_Mu);
-      double pT_AntiMu = _antimuon.pT()/GeV;
-      _hist_MuPlusPt -> fill(pT_AntiMu);
+      const double pT_Mu = _muon.pT()/GeV;
+      _h["MuMinusPt"] -> fill(pT_Mu);
+      const double pT_AntiMu = _antimuon.pT()/GeV;
+      _h["MuPlusPt"] -> fill(pT_AntiMu);
 
-      //// Fill Zboson related histograms
-      double m_Z = (_muon.mom() + _antimuon.mom()).mass()/GeV;
-      _hist_ZM -> fill(m_Z);
+      /// Fill Zboson related histograms
+      const double m_Z = (_muon.mom() + _antimuon.mom()).mass()/GeV;
+      _h["ZM"] -> fill(m_Z);
 
-      double phi_Z = (_muon.mom() + _antimuon.mom()).phi();
-      _hist_ZPhi -> fill(phi_Z);
+      const double phi_Z = (_muon.mom() + _antimuon.mom()).phi();
+      _h["ZPhi"] -> fill(phi_Z);
 
-      double rap_Z = (_muon.mom() + _antimuon.mom()).rap();
-      _hist_ZY -> fill(rap_Z);
+      const double rap_Z = (_muon.mom() + _antimuon.mom()).rap();
+      _h["ZY"] -> fill(rap_Z);
 
-      double pT_Z = (_muon.mom() + _antimuon.mom()).pT()/GeV;
-      _hist_ZPt -> fill(pT_Z);
+      const double pT_Z = (_muon.mom() + _antimuon.mom()).pT()/GeV;
+      _h["ZPt"] -> fill(pT_Z);
 
-      //// Remove jet-muon overlap
-      idiscardIfAnyDeltaRLess(jets, muons, 0.3);
+      //const double pi = 3.14159265358979323846;
+      const double thetastar = acos(tanh((_antimuon.mom().eta() - _muon.mom().eta())/2));
+      const double phistareta = tan(HALFPI - (_antimuon.mom().phi() - _muon.mom().phi())/2)*sin(thetastar);
+      _h["PhiStarEta"] -> fill(phistareta);
 
-      //// Fill jet related histograms
-      _hist_NJets -> fill(jets.size());
+      /// Fill jet related histograms
+      _h["NJets"] -> fill(jets.size());
 
-      _hist_Jet1Phi -> fill(jets.at(0).phi());
-      _hist_Jet2Phi -> fill(jets.at(1).phi());
-      _hist_Jet3Phi -> fill(jets.at(2).phi());
+      _h["Jet1Phi"] -> fill(jets.at(0).phi());
+      _h["Jet2Phi"] -> fill(jets.at(1).phi());
+      _h["Jet3Phi"] -> fill(jets.at(2).phi());
 
-      _hist_Jet1Eta -> fill(jets.at(0).eta());
+      _h["Jet1Eta"] -> fill(jets.at(0).eta());
 
-      _hist_Jet1Y -> fill(jets.at(0).rap());
-      _hist_Jet2Y -> fill(jets.at(1).rap());
-      _hist_Jet3Y -> fill(jets.at(3).rap());
+      _h["Jet1Y"] -> fill(jets.at(0).rap());
+      _h["Jet2Y"] -> fill(jets.at(1).rap());
+      _h["Jet3Y"] -> fill(jets.at(3).rap());
 
-      double pT_aveJet = sum(jets, pT, 0)/GeV/jets.size();
-      _hist_JetAvePt -> fill(pT_aveJet);
+      const double pT_aveJet = sum(jets, pT, 0)/GeV/jets.size();
+      _h["JetAvePt"] -> fill(pT_aveJet);
 
-      _hist_Jet1Pt -> fill(jets.at(0).pT()/GeV);
-      _hist_Jet2Pt -> fill(jets.at(1).pT()/GeV);
-      _hist_Jet3Pt -> fill(jets.at(2).pT()/GeV);
+      _h["Jet1Pt"] -> fill(jets.at(0).pT()/GeV);
+      _h["Jet2Pt"] -> fill(jets.at(1).pT()/GeV);
+      _h["Jet3Pt"] -> fill(jets.at(2).pT()/GeV);
 
       
 
@@ -184,8 +194,11 @@ namespace Rivet {
     /// Normalise histograms etc., after the run
     void finalize() {
 
-      normalize(_h_YYYY); // normalize to unity
-      scale(_h_ZZZZ, crossSection()/picobarn/sumOfWeights()); // norm to cross section
+      //normalize(_h_YYYY); // normalize to unity
+      const double sf = crossSection() / picobarn / sumOfWeights();
+      for (auto hist : _h) {
+        scale(hist.second, sf); // norm to cross section
+      }
 
     }
 
@@ -196,10 +209,11 @@ namespace Rivet {
     //@{
     Histo1DPtr _hist_NMus, _hist_MuPlusPt, _hist_MuPlusEta, _hist_MuPlusPhi, _hist_MuMinusPt, _hist_MuMinusEta, _hist_MuMinusPhi,
               _hist_ZY, _hist_ZM, _hist_ZPhi, _hist_NJets, _hist_Jet1Y, _hist_Jet1Phi, _hist_Jet2Y, _hist_Jet2Phi, _hist_Jet3Y, _hist_Jet3Phi, _hist_MET;
-    Histo1DPtr _hist_ZPt, _hist_Jet1Pt, _hist_Jet2Pt, _hist_Jet3Pt, _hist_JetAvePt, _hist_Jet1Eta;
+    Histo1DPtr _hist_ZPt, _hist_Jet1Pt, _hist_Jet2Pt, _hist_Jet3Pt, _hist_JetAvePt, _hist_Jet1Eta, _hist_PhiStarEta;
 
-    Profile1DPtr _p_AAAA;
-    CounterPtr _c_BBBB;
+    map<string, Histo1DPtr> _h;
+    //Profile1DPtr _p_AAAA;
+    //CounterPtr _c_BBBB;
     //@}
 
 
